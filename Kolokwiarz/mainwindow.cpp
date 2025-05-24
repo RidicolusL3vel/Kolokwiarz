f5f682f6680b70a7c16d44789328d90564e9e720
Jan Bieszczad <ridicoluslvl@gmail.com>
2025-05-24 18:39:01 +0200

    Szkielet designu mainwindow, tytuł z dynamicznym rozmiarem

--- Kolokwiarz/mainwindow.cpp
+++ Kolokwiarz/mainwindow.cpp
@@ -7,6 +7,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QFont font = ui->title->font();
+    // font.setPointSize(-1); // usuń ręczny rozmiar z .ui
+    ui->title->setFont(font);
+
     /*auto source = std::make_unique<JSONQuestionSource>(":/pytania/miernictwo.json");
     quizManager.setQuestionSource(std::move(source));
     quizManager.loadQuestions();
@@ -16,6 +21,16 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+    QMainWindow::resizeEvent(event);
+
+    int fontSize = std::max(72, this->width()/12);
+    QFont font = ui->title->font();
+    font.setPointSize(fontSize);
+    ui->title->setFont(font);
 }
 
 
--- Kolokwiarz/mainwindow.h
+++ Kolokwiarz/mainwindow.h
@@ -8,6 +8,7 @@
 #include "gui\loginwindow.h"
 #include "gui\quizwindow.h"
 #include "gui\rankingwindow.h"
+#include <QResizeEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -23,6 +24,8 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void resizeEvent(QResizeEvent *event) override;
 private:
     Ui::MainWindow *ui;
 
